d8f4fce995137e30ff14d27a78afff5d73533d9f
Yngrid <127627168+yngridc@users.noreply.github.com>
2023-03-13 22:03:05 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,31 @@
+
+const botoesCarrossel = document.querySelectorAll('.botao');
+const imagens = document.querySelectorAll('.imagem');
+
+botoesCarrossel.forEach((botao, indice) => {
+   botao.addEventListener('click', () => {
+
+     desativarBotaoSelecionado();
+
+     botao.classList.add('selecionado');
+
+    esconderImagemAtiva();
+
+    mostrarImagemDeFundo(indice);
+
+
+       function desativarBotaoSelecionado() {
+           const botaoSelecionado = document.querySelector('.selecionado');
+           botaoSelecionado.classList.remove('selecionado');
+       }
+})
+    })
+
+function mostrarImagemDeFundo(indice) {
+    imagens[indice].classList.add('ativa');
+}
+
+function esconderImagemAtiva() {
+    const imagemAtiva = document.querySelector('.ativa');
+    imagemAtiva.classList.remove('ativa');
+}
